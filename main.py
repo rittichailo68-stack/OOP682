1ec9508a48c8b0393e775d36f4e72a263446fbb8
BomNattawut <nattawut.so.65@ubu.ac.th>
2026-01-30 15:41:51 +0700

    almost done?

---
+++ main.py
@@ -0,0 +1,17 @@
+class Dog:
+    def __init__(self, namne, age):
+      self.name = name
+      self.age = age
+    
+    def info(self):
+        print(f"{self.name}is{self.age} yeras old") 
+        
+def main():
+    my_dog = Dog("Buddy", 3)
+    your_dog = Dog ("paulie", 2)
+   
+    print(my_dog)
+
+
+if __name__ == "__main__":
+    main()
