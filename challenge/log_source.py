8e03eed11d4340e4eec7503bf876ea49f1726a06
rittichai <rittichai99@gmail.com>
2026-02-22 19:26:46 +0700

    first commit

---
+++ challenge/log_source.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class ILogSource(ABC):
+    @abstractmethod
+    def get_logs(self):
+        pass
+
+class FileLogSource(ILogSource):
+    def get_logs(self):
+        return ["Log from TXT: System Booting", "Log from TXT: Server Started"]
---
+++ challenge/main.py
@@ -0,0 +1,14 @@
+from log_source import FileLogSource
+from csv_log_source import CsvLogSource
+
+def get_log_source(source_type):
+    if source_type == "file":
+        return FileLogSource()
+    elif source_type == "csv": 
+        return CsvLogSource()
+
+if __name__ == "__main__":
+    source = get_log_source("csv")
+    print("--- Output Log จาก CSV ---")
+    for log in source.get_logs():
+        print(log)
